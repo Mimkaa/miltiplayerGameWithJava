e84d39fe4164f4b391ac00e67eb824a06d363f98
AyanoKira <ayano.kira1@gmail.com>
2025-03-21 14:52:08 +0100

    fixed username issue

--- src/main/java/chat/ChatManager.java
+++ src/main/java/chat/ChatManager.java
@@ -5,9 +5,7 @@ import ch.unibas.dmi.dbis.cs108.example.ClientServerStuff.Message;
 import ch.unibas.dmi.dbis.cs108.example.ClientServerStuff.MessageCodec;
 import ch.unibas.dmi.dbis.cs108.example.ClientServerStuff.Server;
 import javax.swing.SwingUtilities;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -18,11 +16,12 @@ public class ChatManager {
      */
     public static class ClientChatManager {
         private ChatPanel chatPanel;
-        private AtomicReference username;
+        private AtomicReference<String> username;
         private String gameName;
         private ConcurrentLinkedQueue<Message> outgoingQueue;
 
-        public ClientChatManager(AtomicReference username, String gameName, ConcurrentLinkedQueue<Message> outgoingQueue, String uuid) {
+        public ClientChatManager(AtomicReference<String> username, String gameName,
+                                 ConcurrentLinkedQueue<Message> outgoingQueue, String uuid) {
             this.username = username;
             this.gameName = gameName;
             this.outgoingQueue = outgoingQueue;
@@ -35,7 +34,7 @@ public class ChatManager {
                 @Override
                 public void onTyping() {
                     // Optionally, you can implement typing notifications here.
-                    System.out.println(username + " is typing...");
+                    System.out.println(username.get() + " is typing...");
                 }
             });
         }
@@ -46,13 +45,15 @@ public class ChatManager {
 
         /**
          * Wraps the given text into a CHAT message and enqueues it.
-         * The concealed parameters include the sender's username and the game name.
+         * Instead of sending just the text as the sole parameter, we include the sender's username.
+         * The concealed parameters remain fixed.
          */
         public void sendChatMessage(String text) {
-            String usernameString = username.toString();
-            String[] concealedParams = { "something1", "something2" };
-            Message chatMsg = new Message("CHAT", new Object[]{ text }, null, new String[]{ usernameString, gameName });
-            chatMsg.setConcealedParameters(concealedParams);
+            String usernameString = username.get();
+            // Include the username and text as the parameters.
+            Message chatMsg = new Message("CHAT", new Object[]{usernameString, text}, "REQUEST", new String[]{usernameString, gameName});
+            // Overwrite the concealed parameters with the fixed values.
+            chatMsg.setConcealedParameters(new String[]{"gameObject", "gameSession"});
             String encodedMessage = MessageCodec.encode(chatMsg);
             System.out.println("Encoded chat message: " + encodedMessage);
             outgoingQueue.offer(MessageCodec.decode(encodedMessage));
@@ -60,14 +61,13 @@ public class ChatManager {
 
         /**
          * Processes an incoming CHAT message and displays it in the chat panel.
+         * It extracts the sender's name from parameters[0] and the chat text from parameters[1].
          */
         public void processIncomingChatMessage(Message msg, AckProcessor ackProcessor) {
             Object[] params = msg.getParameters();
-            String chatText = (params != null && params.length > 0) ? params[0].toString() : "";
-            // Expect the first concealed parameter to be the sender's name.
-            String senderName = (msg.getConcealedParameters() != null && msg.getConcealedParameters().length > 0)
-                    ? msg.getConcealedParameters()[0] : "Unknown";
-            boolean isLocal = senderName.equals(username);
+            String senderName = (params != null && params.length > 0) ? params[0].toString() : "Unknown";
+            String chatText   = (params != null && params.length > 1) ? params[1].toString() : "";
+            boolean isLocal = senderName.equals(username.get());
             String fullMessage = senderName + ": " + chatText;
             SwingUtilities.invokeLater(() -> chatPanel.appendMessage(fullMessage, isLocal));
             InetSocketAddress socketAddress = new InetSocketAddress("localhost", 9876);
@@ -79,8 +79,6 @@ public class ChatManager {
      * Server-side ChatManager that handles chat messages on the server.
      */
     public static class ServerChatManager {
-
-
         /**
          * Processes an incoming CHAT message:
          * 1. Broadcasts it to all clients.
@@ -90,14 +88,10 @@ public class ChatManager {
             Server server = Server.getInstance();
             // Broadcast the chat message to all clients.
             server.broadcastMessageToAll(msg);
-            // Send an ACK to the sender to prevent repeated retransmission.
             System.out.println("Broadcasted Message to all Clients");
-
-
+            // Send an ACK to the sender.
             ackProcessor.addAck(senderSocket, msg.getUUID());
             System.out.println("Added message UUID " + msg.getUUID() + " to ACK handler");
-
-            }
         }
     }
-
+}
