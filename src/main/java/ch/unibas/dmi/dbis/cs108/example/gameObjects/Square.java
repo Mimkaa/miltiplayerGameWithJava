2bc41f6d9799c5265b8fc4d2e61501aac0b5a687
AyanoKira <ayano.kira1@gmail.com>
2025-04-04 02:42:54 +0200

    add static Platform game object: platform with fixed positon. Update GameObjectFactory to include Platform type. Enable creation of static platforms via CREATEGO messages.

--- src/main/java/ch/unibas/dmi/dbis/cs108/example/gameObjects/Square.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/example/gameObjects/Square.java
@@ -1,5 +1,7 @@
-package ch.unibas.dmi.dbis.cs108.example.ClientServerStuff;
+package ch.unibas.dmi.dbis.cs108.example.gameObjects;
 
+import ch.unibas.dmi.dbis.cs108.example.ClientServerStuff.GameObject;
+import ch.unibas.dmi.dbis.cs108.example.ClientServerStuff.Message;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
